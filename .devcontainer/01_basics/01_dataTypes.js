90cdec4008d19a03386257540dc0370be9ed7476
FarhanRizvvv <153173655+FarhanRizvvv@users.noreply.github.com>
2023-12-07 09:54:32 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS Code as newer version of js//
+
+//alert("hello ") // we are using nodejs, not browser
+// code should be readable 
+// tct9 ecma define the standard of js 
+//for learning use mdn
+
+let name="Farhan"// String ("" or '')
+let age=18      /*Number (range 2^53)
+                 if Number is very large then bigint will be use as datatype*/
+let isLoggedIn //Boolean (True or False)
+               // null is standalone value representation of empty value(value doent get to user)
+               // undefined (when we doesnt assign any value to variable)
+               //symbol use to identify the uniqueness of component
+
+               //object
+console.log(typeof"Farhan");
+//console.log(typeof null); // o/p is object (interview)
+//console.log(typeof undefined); o/p is undefined
+
---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId= 12345
+let accountName= "Farhan"
+var accountPassword= "1223"
+accountCity= "Mumbai"
+var accountCountry;
+// Changing Const value is not allowed that's means we couldnt chanfe it in ahead code//
+ 
+/* let is used to remove redundancy in code , is define in block of scopr only
+   means that we can change the value of same variable name of one code with same 
+   variable name*/
+   /* by default if we doesnt give the data type to variable name then it will 
+   automatically refer it as a var , but it is bad to write as this*/
+// if we doesnt assign any value then it will be undefined//
+console.log(accountId)
+console.table([accountId,accountName,accountPassword,accountCountry,accountCity])
