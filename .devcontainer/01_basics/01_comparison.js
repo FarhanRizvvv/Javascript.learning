a52ff56da53868e8f6f4203db788978955633ee3
FarhanRizvvv <153173655+FarhanRizvvv@users.noreply.github.com>
2023-12-08 17:44:45 +0000

    comparison

---
+++ .devcontainer/01_basics/01_comparison.js
@@ -0,0 +1,22 @@
+/*console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2!=1);*/
+
+/*console.log('2'>'1');
+console.log('2'=='1');
+console.log('2'< '1');
+ treated string as number*/
+
+//console.log("2">1)
+//o/p will true
+
+/*console.log(null>0)
+  console.log(null==0)
+  console.log(null>=0)*/
+// The reason is that an equality check == and comparison ><>=<= work differently.
+// Comparison convert null to a number, treating it as 0
+// Avoid comparison with null undefined bcoz we want clean code
+//=== strict check {check it with datatypes}
+
